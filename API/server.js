e4c8139246b7868dc6acb321392803e2046cb18f
AleksiLe <aleksi.levanen@gmail.com>
2024-11-22 12:18:54 +0200

    Index pages

--- API/server.js
+++ API/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config(); // Load environment variables from .env file
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Initialize the database connection
 const { db } = require('./config/dbConfig');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Function to set up routes dynamically
 const setupRoutes = (dir, baseUrl) => {
