b8570b35113bf16bd08bb9fc634aaf81020e5f6f
AleksiLe <aleksi.levanen@gmail.com>
2025-01-15 19:29:21 +0200

    workflow and build fix

--- front/app/src/app/page.jsx
+++ front/app/src/app/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 export default function Home() {
 
     return (
@@ -5,8 +6,8 @@ export default function Home() {
             <h1 className="text-4xl m-2 font-bold text-center">Shopping made simple for everyone</h1>
             <p className="text-lg m-2 text-center">Welcome to the E-Market</p>
             <div className="flex justify-center">
-                <a href="/products" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-2">Shop Now</a>
-                <a href="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-2">Sign Up</a>
+                <Link href="/products" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-2">Shop Now</Link>
+                <Link href="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-2">Sign Up</Link>
             </div>
         </div>
     );
--- front/app/src/components/navbar/hyperlink.jsx
+++ front/app/src/components/navbar/hyperlink.jsx
@@ -1,13 +1,13 @@
 'use client'
-
+import Link from 'next/link'
 export default function hyperlink(props) {
     if (props.path == props.address) {
         return (
-            <a href={props.address} className="bg-transparent text-blue-700 p-0 dark:text-blue-500" aria-current="page">{props.text}</a>
+            <Link href={props.address} className="bg-transparent text-blue-700 p-0 dark:text-blue-500" aria-current="page">{props.text}</Link>
         );
     } else {
         return (
-            <a href={props.address} className="hover:bg-transparent hover:text-blue-700 p-0 dark:text-white dark:hover:bg-transparent">{props.text}</a>
+            <Link href={props.address} className="hover:bg-transparent hover:text-blue-700 p-0 dark:text-white dark:hover:bg-transparent">{props.text}</Link>
         );
     }  
 }
